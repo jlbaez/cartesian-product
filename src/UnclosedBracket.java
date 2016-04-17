c18a428f2f97c0ed4bcdfb18f7f92df0b983615c
Jose Baez <jlb406@gmail.com>
2016-04-17 15:24:55 -0400

    added exception class

---
+++ src/UnclosedBracket.java
@@ -0,0 +1,10 @@
+/**
+ * Cartesian
+ * Created by jose on 4/17/16.
+ * Last edited on 4/17/16
+ */
+class UnclosedBracket extends Exception {
+    UnclosedBracket(String s) {
+        super(s);
+    }
+}
