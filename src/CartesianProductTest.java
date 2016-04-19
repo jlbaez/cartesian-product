50199b9ee504272af5d152e716caad06f68c3434
Jose Baez <jlb406@gmail.com>
2016-04-18 23:07:56 -0400

    moved tests around

--- src/CartesianProductTest.java
+++ src/CartesianProductTest.java
@@ -3,6 +3,7 @@
  * Created by jose on 4/17/16.
  * Last edited on 4/17/16
  */
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,23 +11,38 @@ import org.junit.rules.ExpectedException;
 import static org.junit.Assert.assertEquals;
 public class CartesianProductTest
 {
+    private CartesianProduct c;
+
+    @Before
+    public void setup() {
+        c = new CartesianProduct();
+    }
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void assertProductCorrect() throws UnclosedBracket {
+    public void assertProductCorrect1() throws UnclosedBracket {
 
-        CartesianProduct c = new CartesianProduct();
         assertEquals("{b,c} test", "b c", c.getProduct("{b,c}"));
+    }
+
+    @Test
+    public void assertProductCorrect2() throws UnclosedBracket {
         assertEquals("a{b,c}d{e,f,g}hi test", "abdehi acdehi abdfhi acdfhi abdghi acdghi", c.getProduct("a{b,c}d{e,f,g}hi"));
+    }
+
+    @Test
+    public void assertProductCorrect3() throws UnclosedBracket {
         assertEquals("a{b,c{d,e,f}g,h}ij{k,l} test", "abijk acdgijk acegijk acfgijk ahijk abijl acdgijl acegijl acfgijl ahijl", c.getProduct("a{b,c{d,e,f}g,h}ij{k,l}"));
+    }
+
+    @Test
+    public void assertProductCorrect4() throws UnclosedBracket {
         assertEquals("c{d,e,f}g test", "cdg ceg cfg", c.getProduct("c{d,e,f}g"));
     }
 
     @Test
     public void assertThrowsBracketError() throws Throwable{
-        CartesianProduct c = new CartesianProduct();
-
         exception.expect(UnclosedBracket.class);
         c.getProduct("c{d,e,f");
     }
